002c32660ebe50ea064e6c2277ecd2ee710c5ece
Atul Kumar <atul.kumar3@go-mmt.com>
2020-06-25 22:29:43 +0530

    added data

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require("express");
+const socketio = require("socket.io");
+
+const expressApp = express();
+
+expressApp.use(express.static(__dirname + "/public"));
+
+const expressServer = expressApp.listen("3031", () => {
+  console.log("Listening on port  3031");
+});
+const io = socketio(expressServer);
+
+io.on("connect", (socket) => {
+  // socket.emit("fromServer", { data: "hello from server" });
+
+  socket.on("msgToServer", (msg) => {
+    io.emit("msgToClients", msg);
+  });
+});
