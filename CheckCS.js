4d7a5eb5c1c058dc3331b1456d9e315398424841
Atul Kumar <atul.kumar3@go-mmt.com>
2020-06-27 15:40:56 +0530

    Delelete data

--- CheckCS.js
+++ CheckCS.js
@@ -1,4 +1,6 @@
 const fetch = require("node-fetch");
+const e = require("express");
+const { response } = require("express");
 
 const PROD_PATH =
   "http://flights-pnr-service.ecs.mmt/flights-pnr-service/v1/mongoCreditShellAmount";
@@ -7,9 +9,22 @@ const DEV_PATH =
   "http://172.16.44.99:9093/flights-pnr-service/v1/mongoCreditShellAmount";
 
 async function getData(pnr, airlinecode) {
-  const response = await fetch(`${DEV_PATH}?airline=${airlinecode}&pnr=${pnr}`);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `${DEV_PATH}?airline=${airlinecode}&pnr=${pnr}`
+    );
+    try {
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      if (response.status === 200) {
+        return "no data found in mongo db";
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    return JSON.stringify(error);
+  }
 }
 
 module.exports = { getData };
